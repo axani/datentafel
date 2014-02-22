d1ee58bf96a20be30fd8a67ea54a4f976d9c4a70
Filip Axani <filip.axani@gmail.com>
2014-02-22 16:20:20 +0100

    Raw api request for music data

--- website/api/music/scripts/domscript.js
+++ website/api/music/scripts/domscript.js
@@ -1,3 +1,38 @@
 $(function() {
 
+    var users = {
+        "Martin": "le_marton", 
+        "Mathias": "dragital",
+        "Philipp": "askaniaskani"
+
+    }
+
+
+    $.each(users, function(real_name, lastfm_name) {
+        
+        /* Get data */
+        $.getJSON( "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + lastfm_name + "&api_key=e1c0d80992681df2296b69b3f73c07f5&format=json", function( data ) {
+            console.log(data);
+            try {
+                data.recenttracks.track[0]['@attr']['nowplaying'];
+                var listening = "listening_active"
+            } catch (e) {
+                var listening = "listening_idle"
+            }
+            var artist = data.recenttracks.track[0].artist['#text'];
+            var songtitle = data.recenttracks.track[0].name
+            var coverimage = data.recenttracks.track[0].image[2]['#text'];
+           
+            /* Create HTML for user */
+            var html_snippet = '<div class="' + listening + ' user_station"><h2>' + real_name + '</h2><strong>' + listening +'</strong><br><img class="coverimage" src="' + coverimage + '" ><br>' + artist + ' - ' + songtitle + '</div>'
+            $('.stations').append(html_snippet)
+            console.log(lastfm_name);
+
+
+        });
+
+
+
+    })
+
 });
